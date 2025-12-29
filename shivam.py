233038c28c6506b1c054f844fb1f2641dcb92423
shivamsadrani <shivamsadrani552@gmail.com>
2025-12-29 11:37:03 +0530

    Add files via upload

---
+++ shivam.py
@@ -0,0 +1,14 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=10
+print(x)
+10
+a = 5
+print(-a)
+-5
+p = 10
+q = 5
+print(p>q)
+True
+print(p==q)
+False
