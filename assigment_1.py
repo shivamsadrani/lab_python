867cd788116e868dc88584c5a5b49a10b10f60fc
shivamsadrani <shivamsadrani552@gmail.com>
2026-01-12 11:39:48 +0530

    Add files via upload

---
+++ assigment_1.py
@@ -0,0 +1,119 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+X="welcome"
+print(X)
+welcome
+print("Rajesh Kumar\n\tFlat No. 101\n\tSunshine Apartment\n\tMG Road\n\tSector 15\n\tRajkot\n\tPincode: 360003")
+Rajesh Kumar
+	Flat No. 101
+	Sunshine Apartment
+	MG Road
+	Sector 15
+	Rajkot
+	Pincode: 360003
+a = 150
+b = 120.50
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+SyntaxError: multiple statements found while compiling a single statement
+ = 150
+b = 120.50
+
+print("Addition=, a + b)
+
+SyntaxError: unexpected indent
+p= float(input("enter the practical amount:"))
+      
+enter the practical amount:1000
+p= float(input("enter the practical amount:"))\nr=float(input("enter the rate of intrest:"))\nt=float(input("enter the time in years:"))\nsi=(p*r*t)/100
+      
+SyntaxError: unexpected character after line continuation character
+p= float(input("enter the practical amount:"))\nr=float(input("enter the rate of intrest:"))\nt=float(input("enter the time in years:"))\nsi=(p*r*t)/100
+      
+SyntaxError: unexpected character after line continuation character
+p= float(input("enter the practical amount:"))
+      \nr=float(input("enter the rate of intrest:"))
+      \nt=float(input("enter the time in years:"))
+      \nsi=(p*r*t)/100
+      
+SyntaxError: multiple statements found while compiling a single statement
+p= float(input("enter the practical amount:"))
+      r=float(input("enter the rate of intrest:"))
+      t=float(input("enter the time in years:"))
+      si=(p*r*t)/100
+      
+SyntaxError: multiple statements found while compiling a single statement
+p= float(input("enter the practical amount:"))
+      
+enter the practical amount:
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    p= float(input("enter the practical amount:"))
+ValueError: could not convert string to float: ''
+r=float(input("enter the rate of intrest:"))
+      
+enter the rate of intrest:10
+  t=float(input("enter the time in years:"))
+      
+SyntaxError: unexpected indent
+print("Rajesh Kumar\n\tFlat No. 101\n\tSunshine Apartment\n\tMG Road\n\tSector 15\n\tRajkot\n\tPincode: 360003")
+      
+Rajesh Kumar
+	Flat No. 101
+	Sunshine Apartment
+	MG Road
+	Sector 15
+	Rajkot
+	Pincode: 360003
+
+KeyboardInterrupt
+KeyboardInterrupt
+print("Rajesh Kumar\n\tFlat No. 101\n\tSunshine Apartment\n\tMG Road\n\tSector 15\n\tRajkot\n\tPincode: 360003")
+
+Rajesh Kumar
+	Flat No. 101
+	Sunshine Apartment
+	MG Road
+	Sector 15
+	Rajkot
+	Pincode: 360003
+name = input("Enter your name: ")
+print("Hello", name)
+      
+SyntaxError: multiple statements found while compiling a single statement
+name = input("Enter your name: ")
+      
+Enter your name: hello
+print("Hello", name)
+      
+Hello hello
+KeyboardInterrupt
+name = input("Enter your name: ")
+
+name = input("Enter your name: ")
+      
+Enter your name: shivam
+print("Hello", name)
+      
+Hello shivam
+
+
+name = input("Enter your name: ")
+      
+Enter your name: samrathsinh
+print("Hello", name)
+
+      
+Hello samrathsinh
+for i in range(1, 6):
+    print(i)
+
+      
+1
+2
+3
+4
+5
